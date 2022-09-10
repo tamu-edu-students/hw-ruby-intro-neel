c26dfe12d9f085351a02a521c2c3672e7609c917
Neel Akash <neelakash@tamu.edu>
2022-09-09 22:20:33 -0500

    HW1 commit
    - added all 3 parts

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,34 +3,53 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  arr.sum {|x| x.to_i}
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  arr.max(2).sum
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  unless arr.length < 2
+    arr.each_with_index { |x, i|
+      arr[i+1..-1].each { |y|
+        return true if x + y == number
+      }
+    }
+  end
+  false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, " + name
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  !!string.match(/^[^\W\daeiou]/i)
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  !!string.match(/^0$|^(0|1)*00$/)
 end
 
 # Part 3
 
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    if isbn == "" || price <= 0.0
+      raise ArgumentError.new
+    end
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    return "$%.2f" % @price
+  end
 end
